e3201fa18ce7be77edce30178df0768b9ac92f69
huiminlim <e0036379@u.nus.edu>
2021-06-10 21:53:13 +0800

    tidy up base code

--- imu_tracking/src/main.c
+++ imu_tracking/src/main.c
@@ -1,9 +1,5 @@
 #include <asf.h>
 
 int main (void) {
-    /* Insert system clock initialization code here (sysclk_init()). */
-
     board_init();
-
-    /* Insert application code here, after the board has been initialized. */
 }
